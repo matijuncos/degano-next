06d830e866b2d900a367da11c98722ffbf8b7051
MJ <MJ>
2024-10-08 13:35:09 -0300

    small adjustments

--- src/components/EditablePayments/EditablePayments.tsx
+++ src/components/EditablePayments/EditablePayments.tsx
@@ -100,14 +100,14 @@ const EditablePayments = () => {
 
   return (
     <>
-      <Box mb='12px' pt='md'>
+      <Box mb='24px' pt='md'>
         <Flex justify='space-between'>
           <Flex
             gap='16px'
             p='10px'
             style={{
               borderRadius: '6px',
-              border: 'solid 2px white'
+              border: 'solid 1px white'
             }}
           >
             <Text fw={600}>Cotizaciòn total del evento</Text>
@@ -133,14 +133,14 @@ const EditablePayments = () => {
         p='10px'
         style={{
           borderRadius: '6px',
-          border: 'solid 2px white'
+          border: 'solid 1px white'
         }}
       >
         <Text fw={600} mb='8px'>
           Pagos parciales:
         </Text>
         <hr />
-        <Box my='12px'>
+        <Box my='24px'>
           <Flex gap='8px'>
             <Text fw={600}>Pago Inicial: </Text>
             <Text>
@@ -207,10 +207,10 @@ const EditablePayments = () => {
       ))}
       <Box
         p='10px'
-        my='16px'
+        my='24px'
         style={{
           borderRadius: '6px',
-          border: 'solid 2px white'
+          border: 'solid 1px white'
         }}
       >
         <Text fw={600} mb='8px'>
